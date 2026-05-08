7508c06a9e7f7b4444c305a3aac60c0d61c89dac
dinabb567-hub <dina.b.b.567@gmail.com>
2026-05-08 22:35:16 +0200

    Add files via upload

--- HolaMundo.cpp
+++ HolaMundo.cpp
@@ -1,15 +1,44 @@
 #include <iostream>
 
-int main() {
+int main() {  
 
-   double pi = 3.14159;
-   double radio;
-   std::cout << "Dime el radio de tu círculo";
-   std::cin >> radio;
-   double circunferencia = 2 * pi * radio;
+   double temp;
+   char op;
 
-   std::cout << "Tu circulo tiene un perímetro de" << circunferencia << " centímetros.";
+   std::cout << "***** conversor de temperatura *****\n";
+   std::cout << "F = Fahrenheit" << std::endl;
+   std::cout << "C = Celsius" << std::endl;
+   std::cout << "A que unidad te gustaría convertir?";
+   std::cin >> op;
+
+   switch(op){
+      case 'f':
+      case 'F':
+      std::cout << "Ingresa la temperatura en Celsius: ";
+      std::cin >> temp;
+
+      temp = 1.8 * temp + 32;
+      std::cout << "La temperatura en Fahrenheit es: " << temp << "\n";
+      break;
+
+      case 'c':
+      case 'C':
+
+      std::cout << "Ingresa la temperatura en Fahrenheit: ";
+      std::cin >> temp;
+
+      temp = (temp - 32) / 1.8;
+      std::cout << "La temperatura en Celsius es: " << temp << "\n";
+      break;
+
+      default:
+      std::cout <<"Ingrese solo c o f\n"; 
+   
+   }
+
+std::cout << "*************************************\n";
 
    return 0;
+
 }
 
