0b3e8cb6d37d8795b6e4a0fd65f6e4a4014a9c9d
dinabb567-hub <dina.b.b.567@gmail.com>
2026-05-11 18:26:19 +0200

    Add files via upload

--- HolaMundo.cpp
+++ HolaMundo.cpp
@@ -1,42 +1,24 @@
 #include <iostream>
 
-int main() {  
+int main() {
 
-   double temp;
-   char op;
+   int filas;
+   int columnas;
+   char símbolo;
 
-   std::cout << "***** conversor de temperatura *****\n";
-   std::cout << "F = Fahrenheit" << std::endl;
-   std::cout << "C = Celsius" << std::endl;
-   std::cout << "A que unidad te gustaría convertir?";
-   std::cin >> op;
+   std::cout << "Ingrese el número de filas: ";
+   std::cin >> filas;
+   std::cout << "Ingrese el número de columnas: ";
+   std::cin >> columnas;
+   std::cout << "Ingrese el símbolo a imprimir: ";
+   std::cin >> símbolo;
 
-   switch(op){
-      case 'f':
-      case 'F':
-      std::cout << "Ingresa la temperatura en Celsius: ";
-      std::cin >> temp;
-
-      temp = 1.8 * temp + 32;
-      std::cout << "La temperatura en Fahrenheit es: " << temp << "\n";
-      break;
-
-      case 'c':
-      case 'C':
-
-      std::cout << "Ingresa la temperatura en Fahrenheit: ";
-      std::cin >> temp;
-
-      temp = (temp - 32) / 1.8;
-      std::cout << "La temperatura en Celsius es: " << temp << "\n";
-      break;
-
-      default:
-      std::cout <<"Ingrese solo c o f\n"; 
-   
+   for (int i = 1; i <= filas; i ++){
+      for (int j = 1; j <= columnas ; j ++){
+     std::cout << símbolo << " ";
+      }
+      std::cout << std::endl;
    }
-
-std::cout << "*************************************\n";
 
    return 0;
 
